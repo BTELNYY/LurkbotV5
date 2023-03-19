1ee1c06b63cfb2879a38794a1212fb209aebaeb8
BTELNYY <btelnyy@gmail.com>
2023-03-18 20:37:24 -0600

    First Push, Frontend

---
+++ discord-bot/LurkbotV5/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LurkbotV5
+{
+    public class Configuration
+    {
+        public Config Config { get; private set; }
+        public void LoadConfiguration(string filename)
+        {
+
+        }
+    }
+
+    public struct Config
+    {
+        public string Token { get; set; }
+        public string Key { get; set; }
+        
+    }
+}
---
+++ discord-bot/LurkbotV5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LurkbotV5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
