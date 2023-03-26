67e097f299ef56346b9ebc09110766d74ec2258e
webmsgr <zapcontact000@gmail.com>
2023-03-25 19:45:10 -0700

    northwood devs breaking lurkbot speedrun (wr)

--- backend/backend/src/northwood.rs
+++ backend/backend/src/northwood.rs
@@ -23,8 +23,7 @@ pub struct SLServer {
 pub struct Player {
     #[serde(rename = "ID")]
     pub id: String,
-    #[serde(default)]
-    pub nickname: String,
+    pub nickname: Option<String>,
 }
 
 pub struct SlServer {
@@ -48,7 +47,7 @@ impl SlServer {
     }
     pub async fn get(&self) -> Result<SLResponse, anyhow::Error> {
         let resp = reqwest::get(self.api_url()).await?.text().await?;
-        //println!("{}", resp);
+        println!("{}", resp);
         let resp: serde_json::Value = serde_json::from_str(&resp)?;
         if resp["Success"].as_bool().unwrap_or(false) {
             let resp: SLResponse = serde_json::from_value(resp)?;
