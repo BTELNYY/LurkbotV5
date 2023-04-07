f8e26abf6d83f576d68365ce8972401e47e19c9f
BTELNYY <btelnyy@gmail.com>
2023-04-07 16:22:28 -0600

    Back to port 8000

--- discord-bot/LurkbotV5/Managers/APIManager.cs
+++ discord-bot/LurkbotV5/Managers/APIManager.cs
@@ -16,7 +16,7 @@ namespace LurkbotV5.Managers
     public static class APIManager
     {
         public static readonly string DevURL = @"http://localhost:8000/";
-        public static readonly string ProdURL = @"http://backend:80/";
+        public static readonly string ProdURL = @"http://backend:8000/";
         public static string CurrentURL { get; private set; } = "?";
 
         public static void Init()
