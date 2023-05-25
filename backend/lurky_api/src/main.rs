69781b767cfc38cee0a7cec93f10cf5ceb9d599e
Wackery <zapcontact000@gmail.com>
2023-05-25 16:53:36 +0000

    monke

--- backend/lurky_api/src/main.rs
+++ backend/lurky_api/src/main.rs
@@ -1,12 +1,15 @@
-use axum::{response::IntoResponse, Router};
+use axum::response::Response;
+use axum::{response::IntoResponse, routing::get, Router};
 use hyper::{StatusCode, Uri};
 use lurky_common::tokio;
 use lurky_common::tracing;
 use lurky_common::tracing::info;
 use lurky_common::tracing::instrument;
+use tower_http::auth::require_authorization::Bearer;
+use tower_http::validate_request::{ValidateRequestHeader, ValidateRequestHeaderLayer};
 
 use std::net::SocketAddr;
-use tower::ServiceBuilder;
+use tower::{ServiceBuilder, Layer};
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::{fmt, EnvFilter};
 #[tokio::main]
@@ -14,7 +17,13 @@ use tracing_subscriber::{fmt, EnvFilter};
 async fn main() {
     let fmt_layer = fmt::layer();
     let filter_layer = EnvFilter::try_from_default_env()
-        .or_else(|_| EnvFilter::try_new("info"))
+        .or_else(|_| {
+            if cfg!(debug_assertions) {
+                EnvFilter::try_new("debug")
+            } else {
+                EnvFilter::try_new("info")
+            }
+        })
         .unwrap();
 
     tracing_subscriber::registry()
@@ -23,10 +32,20 @@ async fn main() {
         .init();
     let middleware = ServiceBuilder::new();
 
-    let app = Router::new().fallback(fallback).layer(middleware);
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/auth", get(auth_test).layer(ValidateRequestHeaderLayer::bearer("gaming")))
+        .fallback(fallback)
+        .layer(middleware);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-    info!("Starting Lurky v{} on {}", env!("CARGO_PKG_VERSION"), addr);
+    info!(
+        "Starting Lurky v{} ({}-{}) on {}",
+        env!("CARGO_PKG_VERSION"),
+        env!("TARGET"),
+        env!("PROFILE"),
+        addr
+    );
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
@@ -35,4 +54,17 @@ async fn main() {
 #[instrument]
 async fn fallback(url: Uri) -> impl IntoResponse {
     (StatusCode::NOT_FOUND, format!("Not found: {}", url.path()))
+}
+#[instrument]
+async fn index() -> impl IntoResponse {
+    format!(
+        "Lurky v{} ({}-{})",
+        env!("CARGO_PKG_VERSION"),
+        env!("TARGET"),
+        env!("PROFILE")
+    )
+}
+#[instrument]
+async fn auth_test() -> impl IntoResponse {
+    "auth ok!"
 }
