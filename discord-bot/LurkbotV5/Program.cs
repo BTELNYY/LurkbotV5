640594dc64f6eb4f4b03d9afff8cbd1cb8bc8de4
BTELNYY <btelnyy@gmail.com>
2023-06-20 14:18:32 -0600

    Command Line

--- discord-bot/LurkbotV5/Program.cs
+++ discord-bot/LurkbotV5/Program.cs
@@ -2,6 +2,7 @@
 using Discord;
 using System;
 using LurkbotV5.Managers;
+using System.Diagnostics;
 
 namespace LurkbotV5
 {
@@ -9,11 +10,13 @@ namespace LurkbotV5
     {
         public static bool CustomConfigPath = false;
         public static string CustomPath = "";
+        static Thread ConsoleThread = new(HandleConsole);
 
         public static Task Main(string[] args) => new Program().MainAsync(args);
 
         public async Task MainAsync(string[] args)
         {
+            ConsoleThread.Start();
             if (args.Contains("--config") && args.Length >= 2)
             {
                 CustomConfigPath = true;
@@ -33,6 +36,30 @@ namespace LurkbotV5
             bot.StartBot();
             TranslationManager.Init();
             await Task.Delay(-1);
+        }
+
+        public static void HandleConsole()
+        {
+            Log.WriteInfo("Started console!");
+            while(ConsoleThread.IsAlive)
+            {
+                string? input = Console.ReadLine();
+                if(string.IsNullOrEmpty(input))
+                {
+                    Log.WriteError("Invalid command.");
+                    continue;
+                }
+                string[] commandParts = input.Split(" ");
+                string command = commandParts[0];
+                switch (command)
+                {
+                    case "addlevelrole":
+                        Log.WriteInfo("Adding level role...");
+                        DiscordManager.AddLevelRole(1, 1033154857948950588, RoleLevelActions.ADD);
+                        break;
+                }
+                continue;
+            }
         }
     }
 }
